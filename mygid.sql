dd47e110e9835b538a7550d53fca60002c93ed23
Clinton Nkefuat <nkefuatclintonngu@gmail.com>
2025-11-30 22:05:26 -0600

    Add files via upload

---
+++ mygid.sql
@@ -0,0 +1,10 @@
+CREATE TABLE persons (
+id INT NOT NULL,
+person_name VARCHAR (50) NOT NULL,
+birth_date DATE,
+phone VARCHAR (15) NOT NULL,
+CONSTRAINT pk_persons PRIMARY KEY (id)
+) 
+
+SELECT *
+FROM persons
